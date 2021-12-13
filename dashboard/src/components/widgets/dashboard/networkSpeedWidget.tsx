cc5776f31d44e4c686b0d028156e1722670257ac
QuentinFringhian <quentin.fringhian@epitech.eu>
2021-12-13 17:36:22 +0100

    feat: networkStatus mockup data

--- dashboard/src/components/widgets/dashboard/networkSpeedWidget.tsx
+++ dashboard/src/components/widgets/dashboard/networkSpeedWidget.tsx
@@ -1,4 +1,11 @@
+import { useState, useEffect } from "react";
+
 import CustomCard from "../../utils/customCard";
+
+import UploadIcon from "../../../assests/icon/upload.svg";
+import DownloadIcon from "../../../assests/icon/download.svg";
+
+import { NetworkStatus, getNetworkStatus } from "../../../utils/api/network";
 
 type NetworkSpeedWidgetProps = {
   style?: {
@@ -13,19 +20,45 @@ type NetworkSpeedWidgetProps = {
  */
 
 function NetworkSpeedWidget({ style }: NetworkSpeedWidgetProps): JSX.Element {
+  const [networkStatus, setNetworkStatus] = useState<NetworkStatus | null>(
+    null
+  );
+
+  const updateNetworkStatus = () => {
+    getNetworkStatus().then(setNetworkStatus).catch(console.error);
+  };
+
+  useEffect(() => {
+    updateNetworkStatus();
+    const updateNetworkStatusInterval = setInterval(updateNetworkStatus, 5000);
+    return () => clearInterval(updateNetworkStatusInterval);
+  }, []);
+
   return (
     <CustomCard
       title="Network Speed"
-      subTitle="Stable"
+      subTitle={networkStatus?.status}
       style={{ className: style?.className }}
     >
-      <div className="flex flex-row">
+      <div className="flex flex-row items-center">
         <CustomCard
-          style={{ backgroundColor: "bg-gray-200", className: "w-32 h-20" }}
+          style={{
+            backgroundColor: "bg-networkSpeedDownload",
+            padding: "pt-5 pb-5 pl-4 pr-4",
+          }}
+          icon={DownloadIcon}
+          title="Download"
+          subTitle={networkStatus?.downloadSpeed + " Mb/s"}
         />
         <div className="w-12" />
         <CustomCard
-          style={{ backgroundColor: "bg-gray-200", className: "w-32 h-20" }}
+          style={{
+            backgroundColor: "bg-networkSpeedUpload",
+            padding: "pt-5 pb-5 pl-4 pr-4",
+          }}
+          icon={UploadIcon}
+          title="Upload"
+          subTitle={networkStatus?.uploadSpeed + " Mb/s"}
         />
       </div>
     </CustomCard>
