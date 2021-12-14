4f90cd4c754d9b5a73b939c7d271a89ee166d62b
Tom Chauveau <tom.chauveau@epitech.eu>
2021-12-14 17:22:42 +0100

    Add missing svg and improve import statements
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

---
+++ dashboard/src/components/widgets/dashboard/index.ts
@@ -0,0 +1,17 @@
+import DashboardWidget from './dashboardWidget';
+import HardwareStatusWidget from './hardwareStatusWidget';
+import LastSuspisciousPackagesWidget from './lastSuspisciousPackagesWidget';
+import NetworkSpeedWidget from './networkSpeedWidget';
+import OriginSuspisciousPacketsWidget from './originSuspisciousPacketsWidget';
+import RulesWidget from './rulesWidget';
+import UserConnectedWidget from './userConnectedWidget';
+
+export {
+	DashboardWidget,
+	HardwareStatusWidget,
+	LastSuspisciousPackagesWidget,
+	NetworkSpeedWidget,
+	OriginSuspisciousPacketsWidget,
+	RulesWidget,
+	UserConnectedWidget
+}
